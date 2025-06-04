6e97151c9b0dc129e3208e6a689ca6539e0f4427
Serhii-Mazurenko376 <214352403+Serhii-Mazurenko376@users.noreply.github.com>
2025-06-04 22:37:34 +0200

    Dag 1 - Weather checker övning klart

---
+++ Week_1/weather_checker.py
@@ -0,0 +1,8 @@
+temp = int(input("Vad är temperaturen ute? "))
+
+if temp > 25:
+    print("Det är varmt! Ta på shorts.")
+elif temp > 10:
+    print("Lite kyligt. Ta en jacka.")
+else:
+    print("Kallt! Klä dig varmt.")
